266d4f77137affab6ace4fe909a18f971c818426
Uochukwu <jamesegeonu003@gmail.com>
2022-11-09 14:25:07 +0100

    Creating express server

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-// building an express app
-const app = express();
-
-// listen for requests
-app.listen(3000, () => {
-    console.log(`listening on port: 3000`);
-})
