3b76f53a2b74407678ac15e12db1949957446397
Uochukwu <jamesegeonu003@gmail.com>
2022-11-10 23:26:55 +0100

    added partials template and styling

--- index.js
+++ index.js
@@ -12,18 +12,23 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'How to use chrome developer tools', snippet: 'lorem ipsum dolor sit amet, consectetur'},
+        {title: 'Creating a product that sells', snippet: 'lorem ipsum dolor sit amet, consectetur'},
+        {title: 'Inside story: Meta staff layoff', snippet: 'lorem ipsum dolor sit amet, consectetur'},
+    ];
+    res.render('index', { title: ' Home ', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: ' About '});
 });
 
 // handler function
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a new blog '});
 });
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: ' 404 '});
 });
